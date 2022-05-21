c4825ccf9d90f08fa586ccc66746d5248de9612f
anhtu95 <anhtu23795@gmail.com>
2022-05-21 09:55:36 +0200

    update requirement and bash

--- bash/run_training.sh
+++ bash/run_training.sh
@@ -6,8 +6,6 @@ else
   python3 -m venv /workspace/students/anhtu/transformers-text-recognition/venv
   source /workspace/students/anhtu/transformers-text-recognition/venv/bin/activate
   pip install -r /workspace/students/anhtu/transformers-text-recognition/requirements.txt
-  python -m spacy download en_core_web_sm
-  python -m spacy download de_core_news_sm
 fi
 python /workspace/students/anhtu/transformers-text-recognition/training.py
 deactivate
